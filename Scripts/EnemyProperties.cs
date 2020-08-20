a4fd16bbbd34f58eb8fd9515af86d172982d7ea0
BrandonComins <brandcomin@gmail.com>
2020-08-20 02:50:00 -0700

    fix collision issue with heart, and make enemies have 20% chance to drop heart

--- Scripts/EnemyProperties.cs
+++ Scripts/EnemyProperties.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class EnemyProperties : MonoBehaviour {
    
+	public Transform enemy;
+    public GameObject heartPrefab;
+	public float healthDropChance;
+	
     public int health = 40;
+
       
 	private Rigidbody2D rb;
+
+
+	void Awake(){
+		healthDropChance = Random.Range(0f, 1f);
+		rb = transform.GetComponent<Rigidbody2D>();
+
+	}
 
     public void update(){
     
 	
-	}
-
-	void Awake(){
-		rb = transform.GetComponent<Rigidbody2D>();
-		
 	}
 
    	public void takeDamage(int damage){
@@ -28,7 +35,17 @@ public class EnemyProperties : MonoBehaviour {
 
       private void die(){
    		Destroy(gameObject);
+		dropHealth();
+		
    	}  
+
+	private void dropHealth(){
+		Debug.Log(healthDropChance);
+		if(healthDropChance <= .2f){
+			GameObject heart = Instantiate(heartPrefab, enemy.position, enemy.rotation);
+		}
+	}
+
 }
       
     
